bd50417742ed6d84421840b85290351623d1ab9e
devevandro <dev.evandro@gmail.com>
2022-05-28 10:01:32 -0300

    feat: add configs.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import Head from "next/head";
+import { theme } from '@/presentation/styles/theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Head>
         <title>Project Example</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
